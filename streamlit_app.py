20c61b0677286100f6c06bc1d658b7701f5cbf82
shimarock <shimada@unerry.co.jp>
2023-07-29 21:08:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,41 @@
 import streamlit as st
-from datetime import time, datetime
+from datetime import datetime, time
 
-st.header('st.slider')
+# サイドバーで選択肢を設定
+option = st.sidebar.selectbox(
+    'Choose an example:',
+    ('Example 1: Slider', 'Example 2: Range slider', 'Example 3: Range time slider', 'Example 4: Datetime slider')
+)
 
-#例1
+# 例1
+if option == 'Example 1: Slider':
+    st.subheader('Slider')
+    age = st.slider('How old are you?', 0, 130, 25)
+    st.write("I'm ", age, 'years old')
 
-st.subheader('Slider')
+# 例2
+elif option == 'Example 2: Range slider':
+    st.subheader('Range slider')
+    values = st.slider(
+         'Select a range of values',
+         0.0, 100.0, (25.0, 75.0))
+    st.write('Values:', values)
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+# 例3
+elif option == 'Example 3: Range time slider':
+    st.subheader('Range time slider')
+    appointment = st.slider(
+         "Schedule your appointment:",
+         value=(time(11, 30), time(12, 45)))
+    st.write("You're scheduled for:", appointment)
 
-#例2
-
-st.subheader('Range slider')
-
-values = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
-
-#例3
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 30), time(12, 45)))
-st.write("You're scheduled for:", appointment)
-
-#例4
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-     "When do you start?",
-     min_value=datetime(2020, 1, 1, 0, 0),
-     max_value=datetime(2020, 12, 31, 23, 59),
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-
-st.write("Start time:", start_time)
+# 例4
+elif option == 'Example 4: Datetime slider':
+    st.subheader('Datetime slider')
+    start_time = st.slider(
+         "When do you start?",
+         min_value=datetime(2020, 1, 1, 0, 0),
+         max_value=datetime(2020, 12, 31, 23, 59),
+         value=datetime(2020, 1, 1, 9, 30),
+         format="MM/DD/YY - hh:mm")
+    st.write("Start time:", start_time)
