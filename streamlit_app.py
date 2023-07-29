2a76782d380073724ce45504bedb13f72902a905
shimarock <shimada@unerry.co.jp>
2023-07-29 21:43:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,36 @@
 import streamlit as st
 from datetime import datetime, time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+import plotly.express as px
+import pydeck as pdk
+import graphviz as gv
+
+# グラフの選択肢
+graphs = [
+    "Line Chart",
+    "Area Chart",
+    "Bar Chart",
+    "Matplotlib Plot",
+    "Altair Chart",
+    "Vega Lite Chart",
+    "Plotly Chart",
+    "Bokeh Chart",
+    "Pydeck Chart",
+    "Graphviz Chart",
+    "Map",
+]
 
 # サイドバーで選択肢を設定
 option = st.sidebar.selectbox(
     'Choose an example:',
     ('Example 1: Slider', 'Example 2: Range slider', 'Example 3: Range time slider', 'Example 4: Datetime slider')
 )
+
+# サイドバーでグラフの選択
+selected_graph = st.sidebar.selectbox("Choose a graph:", graphs)
 
 # 例1
 if option == 'Example 1: Slider':
@@ -43,3 +68,44 @@ elif option == 'Example 4: Datetime slider':
          value=datetime(2020, 1, 1, 9, 30),
          format="MM/DD/YY - hh:mm")
     st.write("Start time:", start_time)
+
+# サンプルデータの生成
+np.random.seed(42)
+data = np.random.randn(100, 3)
+df = pd.DataFrame(data, columns=["A", "B", "C"])
+
+# 選択されたグラフの描画
+if selected_graph == "Line Chart":
+    st.line_chart(df)
+elif selected_graph == "Area Chart":
+    st.area_chart(df)
+elif selected_graph == "Bar Chart":
+    st.bar_chart(df)
+elif selected_graph == "Matplotlib Plot":
+    fig, ax = plt.subplots()
+    ax.plot(df["A"])
+    st.pyplot(fig)
+elif selected_graph == "Altair Chart":
+    chart = alt.Chart(df).mark_line().encode(x='A', y='B')
+    st.altair_chart(chart)
+elif selected_graph == "Vega Lite Chart":
+    st.vega_lite_chart(df, {"mark": "line", "encoding": {"x": "A", "y": "B"}})
+elif selected_graph == "Plotly Chart":
+    fig = px.line(df, x="A", y="B")
+    st.plotly_chart(fig)
+elif selected_graph == "Bokeh Chart":
+    # Bokehの例を追加
+    pass
+elif selected_graph == "Pydeck Chart":
+    view_state = pdk.ViewState(latitude=37.7749, longitude=-122.4194, zoom=10, pitch=0)
+    r = pdk.Deck(map_style="mapbox://styles/mapbox/light-v9", initial_view_state=view_state)
+    st.pydeck_chart(r)
+elif selected_graph == "Graphviz Chart":
+    dot = gv.Digraph()
+    dot.node('A')
+    dot.node('B')
+    dot.edge('A', 'B')
+    st.graphviz_chart(dot)
+elif selected_graph == "Map":
+    map_data = pd.DataFrame({'lat': [37.7749], 'lon': [-122.4194]})
+    st.map(map_data)
